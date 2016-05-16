a02dcb442b9c8a3b479d0b4dbfdc71ec12e8032b
BeamWD <bastowebdev@gmail.com>
2016-05-16 17:30:46 +0200

    Renommage fichiers + admin

--- admin.php
+++ admin.php
@@ -1,4 +1,55 @@
+
+<!doctype html>
+<html>
+<head>
+<title>Bienvenue chez Minibu !</title>
+<meta charset="utf-8" />
+<meta http-equiv="x-ua-compatible" content="ie=edge" />
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+<meta name="keywords" content="Minibu,coiffeur,coiffure,hommes,sans,rendez-vous,Châtelet,Chatelet,place,saint-roch,"/>
+<meta name="description" content="Chez Minibu, coiffure hommes - 000 Place Saint-Roch 000 Châtelet" />
+
 <?php
-echo "Page admin <br/>"
-echo "Circulez y a rien à voir";
+  require('header.php')
 ?>
+
+</head>
+<body>
+<?php
+  $msg="Entrer mot de passe"
+?>
+<form class ="form-signin"
+  role ="form"
+  action ="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>"
+  method ="post">
+  <h1 class = "form-signin-heading">
+    <?php echo $msg; ?>
+  </h1>
+  <label>
+    Utilisateur
+  </label>
+  <input type="text"
+    class="form-control" 
+    name="username"
+    placeholder="username=tutorialspoint" 
+    required
+    autofocus>
+  </br>
+  <label>
+    Mot de passe
+  </label>
+  <input type="password"
+    class="form-control"
+    name="password"
+    placeholder="password=1234"
+    required>
+  <br />
+  <button class = "btn btn-lg btn-primary btn-block"
+    type = "submit" 
+    name = "login">
+    Login
+  </button>
+</form>
+<body>
+</html>
---
+++ header.php
@@ -0,0 +1,52 @@
+<!--
+<link rel="stylesheet" lang="text/css" href="" />
+-->
+
+<!-- For IE 10 and below -->
+<!-- No link, just place a file called favicon.ico in the root directory -->
+
+<!-- For IE 11, Chrome, Firefox, Safari, Opera -->
+<link rel="icon" href="path/to/favicon-16.png" sizes="16x16" type="image/png" />
+<link rel="icon" href="path/to/favicon-32.png" sizes="32x32" type="image/png" />
+<link rel="icon" href="path/to/favicon-48.png" sizes="48x48" type="image/png" />
+<link rel="icon" href="path/to/favicon-62.png" sizes="62x62" type="image/png" />
+
+<link rel="stylesheet" href="bootstrap.css" />
+<link rel="stylesheet" href="bootstrap-theme.css" />
+
+<!-- TODO : facebok open graph markup -->
+<!-- Google + ? -->
+
+<style> /*colors*/
+#header, #footer {
+	background-color: lightgreen;
+}
+</style>
+
+<style> /*layout*/
+html, body, #map {
+	margin: 0 auto;
+	padding: 0 auto;
+	text-align: center;
+}
+#footer {
+	bottom: 0;
+	position: fixed;
+	width: 100%;
+}
+#main {
+	margin: 1em;
+}
+#map {
+	clear: both;
+	border: 1px solid lightgreen;
+  width: 325px;
+  height: 300px;
+}
+</style>
+
+<style> /*text*/
+</style>
+
+<style> /*other*/
+</style>
